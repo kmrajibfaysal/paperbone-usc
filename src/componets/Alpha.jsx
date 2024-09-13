fa8646519d73ee6755fc464695ab6554fc83d700
kmrajibfaysal <kmrajibfaysal@gmail.com>
2024-09-13 23:50:55 +0600

    added new files

--- src/componets/Alpha.jsx
+++ src/componets/Alpha.jsx
@@ -1,11 +1,16 @@
-
-
 const Alpha = () => {
-    return (
-        <div>
-            <h1>working!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>working!</h1>
+    </div>
+  );
 };
 
 export default Alpha;
+
+
+
+
+
+// what is ther 
+// --- what ther hack is going on andtjawlejflsadjfoj4tojwefljwepo el
